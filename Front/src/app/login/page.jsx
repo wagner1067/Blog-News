22a48164367c0b1e3621ad17cb020770dfe04693
wagner1067 <barra-wag@hotmail.com>
2024-04-10 23:20:34 -0300

    criando a tela de login

---
+++ Front/src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "../../components/LoginForm/index";
+
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
+
+export default LoginPage;
