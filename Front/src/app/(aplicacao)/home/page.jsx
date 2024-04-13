1aa13632b0fb87e2a556dc1b7a5d7aa232af229a
wagner1067 <barra-wag@hotmail.com>
2024-04-13 13:09:35 -0300

    criando a tela de Noticia

--- Front/src/app/(aplicacao)/home/page.jsx
+++ Front/src/app/(aplicacao)/home/page.jsx
@@ -1,9 +1,34 @@
+import Noticia from "@/components/Noticia";
+
+const noticias = [
+    {
+        id: 1,
+        titulo: 'Noticia 1 ',
+        img: 'https://via.placeholder.com/150',
+        texto: 'Texto da <b>noticia 1</b>',
+    },
+    {
+        id: 2,
+        titulo: 'Noticia 2 ',
+        img: 'https://via.placeholder.com/150',
+        texto: 'Texto da <b>noticia 2</b>',
+    },
+    {
+        id: 3,
+        titulo: 'Noticia 3 ',
+        img: 'https://via.placeholder.com/150',
+        texto: 'Texto da <b>noticia 3</b>',
+    },
+];
+
 const HomePage = () => {
     return (
         <div>
             <h1>
                 Home
             </h1>
+            {noticias.map(noticia =>
+                <Noticia key={noticia.id} noticia={noticia} />)}
         </div>
     )
 };
